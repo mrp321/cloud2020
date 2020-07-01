afeb5185a0099f3ddc6f4b48e01b5b998f207d72
qchen <y7654f@gmail.com>
2020-07-02 02:49:11 +0800

    16.1. SpringCloud Sleuth 分布式链路跟踪-支付服务提供者cloud-provider-payment8001(链路跟踪) 16.2. SpringCloud Sleuth 分布式链路跟踪-订单服务消费者cloud-consumer-order80(链路跟踪)

--- cloud-consumer-order80/src/main/java/cn/sitedev/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/cn/sitedev/springcloud/controller/OrderController.java
@@ -69,4 +69,11 @@ public class OrderController {
         URI uri = serviceInstance.getUri();
         return restTemplate.getForObject(uri + "/payment/lb", String.class);
     }
+
+    // ====================> zipkin+sleuth
+    @GetMapping("/zipkin")
+    public String paymentZipkin() {
+        String result = restTemplate.getForObject("http://localhost:8001" + "/payment/zipkin/", String.class);
+        return result;
+    }
 }
